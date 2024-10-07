eb411b8cf51dc84c52dd2a4266c9993436d12b81
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-10-07 13:26:37 +0200

    Update 01.10.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,24 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Application {
 
+	private static JFrame root;
+	private static JTextField textField1;
+	private static JTextField textField2;
+	private static JLabel jLabel;
+
 	public static void main(String[] args) {
-		JFrame root = new JFrame("DateDiff");
-		root.setLayout(new FlowLayout());
+		root = new JFrame("DateDiff");
+		root.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT,20,20));
 
-		JTextField txf1 = new JTextField(6);
-		JTextField txf2 = new JTextField(6);
-		JButton btnBerechnen = new JButton("berechne");
-		JLabel lblErgebnis = new JLabel("???");
+		textField1 = new JTextField(6);
+		textField2 = new JTextField(6);
+		root.getContentPane().add(textField1);
+		root.getContentPane().add(textField2);
 
-		root.getContentPane().add(txf1);
-		root.getContentPane().add(txf2);
-		root.getContentPane().add(btnBerechnen);
-		root.getContentPane().add(lblErgebnis);
+		JButton jButton = new JButton("berechne");
+		jButton.addActionListener(new ButtonEventHandler());
+		root.getContentPane().add(jButton);
+
+		jLabel = new JLabel("???");
+		root.getContentPane().add(jLabel);
 
 		root.pack();
 		root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		root.setVisible(true);
+	}
+
+	private static class ButtonEventHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+			try {
+				Date date1 = formatter.parse(textField1.getText());
+				Date date2 = formatter.parse(textField2.getText());
+				long diffInMillies = date2.getTime() - date1.getTime();
+				long diffInDays = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+				jLabel.setText(String.format("%d Tage", diffInDays));
+				root.pack();
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
 	}
 }
