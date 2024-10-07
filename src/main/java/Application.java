fda82cb1fe61a2b0b370ba3c088070e97454b629
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-10-07 13:33:29 +0200

    Rename Fields

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,25 +9,25 @@ import java.util.concurrent.TimeUnit;
 public class Application {
 
 	private static JFrame root;
-	private static JTextField textField1;
-	private static JTextField textField2;
-	private static JLabel jLabel;
+	private static JTextField jtfDatum1;
+	private static JTextField jtfDatum2;
+	private static JLabel lblErgebnis;
 
 	public static void main(String[] args) {
 		root = new JFrame("DateDiff");
 		root.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT,20,20));
 
-		textField1 = new JTextField(6);
-		textField2 = new JTextField(6);
-		root.getContentPane().add(textField1);
-		root.getContentPane().add(textField2);
+		jtfDatum1 = new JTextField(6);
+		jtfDatum2 = new JTextField(6);
+		root.getContentPane().add(jtfDatum1);
+		root.getContentPane().add(jtfDatum2);
 
 		JButton jButton = new JButton("berechne");
 		jButton.addActionListener(new ButtonEventHandler());
 		root.getContentPane().add(jButton);
 
-		jLabel = new JLabel("???");
-		root.getContentPane().add(jLabel);
+		lblErgebnis = new JLabel("???");
+		root.getContentPane().add(lblErgebnis);
 
 		root.pack();
 		root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -39,11 +39,11 @@ public class Application {
 		public void actionPerformed(ActionEvent e) {
 			SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 			try {
-				Date date1 = formatter.parse(textField1.getText());
-				Date date2 = formatter.parse(textField2.getText());
+				Date date1 = formatter.parse(jtfDatum1.getText());
+				Date date2 = formatter.parse(jtfDatum2.getText());
 				long diffInMillies = date2.getTime() - date1.getTime();
 				long diffInDays = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
-				jLabel.setText(String.format("%d Tage", diffInDays));
+				lblErgebnis.setText(String.format("%d Tage", diffInDays));
 				root.pack();
 			} catch (Exception exception) {
 				exception.printStackTrace();
