9215f7e692031c0cc5d126f76f1365f23c66bf6d
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-10-14 15:14:33 +0200

    Endstand

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -13,25 +16,40 @@ public class Application {
 	private static JTextField jtfDatum1;
 	private static JTextField jtfDatum2;
 	private static JLabel lblErgebnis;
+	private static Label lastTen;
+	private static String[] listLastTen = new String [10];
 
 	public static void main(String[] args) {
 		root = new JFrame("DateDiff");
 		root.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT,20,20));
 
+		Box mainBox = Box.createVerticalBox();
+
+		Box firstRow = Box.createHorizontalBox();
+		mainBox.add(firstRow);
+		firstRow.add(new Label("1. Datum:"));
+
+		Box secondRow = Box.createHorizontalBox();
+		mainBox.add(secondRow);
+		secondRow.add(new Label("2. Datum:"));
+
+		root.getContentPane().add(mainBox);
+
 		jtfDatum1 = new JTextField(6);
 		jtfDatum1.setName("Datum1");
 		jtfDatum1.getDocument().putProperty("owner", jtfDatum1);
 		jtfDatum1.getDocument().addDocumentListener(new DateCheckEventHandler());
-		root.getContentPane().add(jtfDatum1);
+		firstRow.add(jtfDatum1);
 
 		jtfDatum2 = new JTextField(6);
 		jtfDatum2.setName("Datum2");
 		jtfDatum2.setEnabled(false);
 		jtfDatum2.getDocument().putProperty("owner", jtfDatum2);
 		jtfDatum2.getDocument().addDocumentListener(new DateCheckEventHandler());
-		root.getContentPane().add(jtfDatum2);
+		secondRow.add(jtfDatum2);
 
 		JButton jButton = new JButton("berechne");
+		jButton.setName("Button");
 		jButton.addActionListener(new ButtonEventHandler());
 		jButton.setEnabled(false);
 		root.getContentPane().add(jButton);
@@ -39,12 +57,19 @@ public class Application {
 		lblErgebnis = new JLabel("???");
 		root.getContentPane().add(lblErgebnis);
 
+		lastTen = new Label("Sammlung");
+		//lastTen.setEditable(false);
+		lastTen.addMouseWheelListener(new ChangeLastTenEventHandler());
+		root.getContentPane().add(lastTen);
+
 		root.pack();
 		root.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		root.setVisible(true);
 	}
 
 	private static class ButtonEventHandler implements ActionListener {
+
+		private int counter = 0;
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
@@ -53,13 +78,50 @@ public class Application {
 				Date date2 = formatter.parse(jtfDatum2.getText());
 				long diffInMillies = date2.getTime() - date1.getTime();
 				long diffInDays = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
-				if (diffInDays == 1 || diffInDays == -1)
+				if (diffInDays == 1 || diffInDays == -1) {
 					lblErgebnis.setText(String.format("%d Tag", diffInDays));
-				else
+					if (counter < 10) {
+						listLastTen[counter++] = String.format("%s - %s = %d Tag", jtfDatum1.getText(), jtfDatum2.getText(), diffInDays);
+						lastTen.setText(listLastTen[counter - 1]);
+					} else {
+						String[] buf = new String[10];
+						System.arraycopy(listLastTen,1, buf, 0, 9);
+						listLastTen = buf;
+						listLastTen[9] = String.format("%s - %s = %d Tag", jtfDatum1.getText(), jtfDatum2.getText(), diffInDays);
+						lastTen.setText(listLastTen[9]);
+					}
+				} else {
 					lblErgebnis.setText(String.format("%d Tage", diffInDays));
+					if (counter < 10) {
+						listLastTen[counter++] = String.format("%s - %s = %d Tage", jtfDatum1.getText(), jtfDatum2.getText(), diffInDays);
+						lastTen.setText(listLastTen[counter - 1]);
+					} else {
+						String[] buf = new String[10];
+						System.arraycopy(listLastTen,1, buf, 0, 9);
+						listLastTen = buf;
+						listLastTen[9] = String.format("%s - %s = %d Tage", jtfDatum1.getText(), jtfDatum2.getText(), diffInDays);
+						lastTen.setText(listLastTen[9]);
+					}
+				}
 				root.pack();
+				System.out.println(Arrays.deepToString(listLastTen));
 			} catch (Exception exception) {
 				exception.printStackTrace();
+			}
+		}
+	}
+
+	private static class ChangeLastTenEventHandler implements MouseWheelListener {
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			int index;
+			for (index = 0; index < listLastTen.length; index++) {
+				if (listLastTen[index] != null && listLastTen[index].equals(lastTen.getText())) break;
+			}
+			index = index + e.getWheelRotation();
+			if (index >= 0 && index < 10) {
+				if (listLastTen[index] != null)
+					lastTen.setText(listLastTen[index]);
 			}
 		}
 	}
--- src/main/java/DateCheckEventHandler.java
+++ src/main/java/DateCheckEventHandler.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
+import java.awt.*;
 
 public class DateCheckEventHandler implements DocumentListener {
 	@Override
@@ -9,16 +10,30 @@ public class DateCheckEventHandler implements DocumentListener {
 		String regex = "^\\d{2}\\.\\d{2}\\.\\d{4}$";
 		try {
 			if (e.getDocument().getProperty("owner") instanceof JTextField jtf) {
+				JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(jtf);
 				switch (jtf.getName()) {
 					case "Datum1":
-						jtf.getParent()
-								.getComponent(1)
-								.setEnabled(e.getDocument().getText(0, e.getDocument().getLength()).matches(regex));
+						for (Component comp : frame.getContentPane().getComponents()) {
+							if (comp instanceof Box box1) {
+								for (Component boxComp1 : box1.getComponents()) {
+									if (boxComp1 instanceof Box box2) {
+										for (Component boxComp2 : box2.getComponents()) {
+											if ("Datum2".equals(boxComp2.getName())) {
+												boxComp2.setEnabled(e.getDocument().getText(0, e.getDocument().getLength()).matches(regex));
+											}
+										}
+									}
+								}
+							}
+						}
 						break;
 					case "Datum2":
-						jtf.getParent()
-								.getComponent(2)
-								.setEnabled(e.getDocument().getText(0, e.getDocument().getLength()).matches(regex));
+						for (Component comp : frame.getContentPane().getComponents()) {
+							if (comp instanceof JButton btn) {
+								if ("Button".equals(btn.getName()))
+									btn.setEnabled(e.getDocument().getText(0, e.getDocument().getLength()).matches(regex));
+							}
+						}
 						break;
 				}
 			}
